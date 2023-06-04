02d6ce84d71970a72f1e379d4e525df4cfab149d
USERSAD\jcardero <David Rodriguez>
2023-06-04 13:41:42 -0500

    add perfil

--- sprint-boot-web-david/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ sprint-boot-web-david/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,19 +1,37 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.security.KeyStore.Entry.Attribute;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index", "/", "/home"})
+	@GetMapping({"/index", "","/", "/home"})
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Framework");
-		
+		model.addAttribute("titulo", "Hola Spring Framework");		
 		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNombre("David");
+		usuario.setApellido("Rodriguez");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));	
+		
+		return "prefil";
 	}
 }
 
-//return dev 1
+
