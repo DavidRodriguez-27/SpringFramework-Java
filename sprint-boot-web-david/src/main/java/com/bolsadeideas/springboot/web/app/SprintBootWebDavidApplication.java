2c0bdb60e6871376c94c47264226898de8bafda1
USERSAD\jcardero <David Rodriguez>
2023-06-04 11:09:56 -0500

    primer commit

---
+++ sprint-boot-web-david/src/main/java/com/bolsadeideas/springboot/web/app/SprintBootWebDavidApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootWebDavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootWebDavidApplication.class, args);
+	}
+
+}
